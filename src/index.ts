000eccf014b141df48d64b81974c0dbfe2f40a67
Shigma <shigma10826@gmail.com>
2024-10-28 00:34:55 +0800

    chore: bump versions

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,19 @@ import {} from '@koishijs/plugin-console'
 import { resolve } from 'path'
 
 export const name = 'vanilla'
-export const using = ['console']
+export const inject = ['console']
 
 export interface Config {}
 export const Config: z<Config> = z.object({})
 
 export function apply(ctx: Context, config: Config) {
-  ctx.console.addEntry({
+  ctx.console.addEntry(process.env.KOISHI_BASE ? [
+    process.env.KOISHI_BASE + '/dist/index.js',
+    process.env.KOISHI_BASE + '/dist/style.css',
+  ] : process.env.KOISHI_ENV === 'browser' ? [
+    // @ts-ignore
+    import.meta.url.replace(/\/src\/[^/]+$/, '/client/index.ts'),
+  ] : {
     dev: resolve(__dirname, '../client/index.ts'),
     prod: resolve(__dirname, '../dist'),
   })
